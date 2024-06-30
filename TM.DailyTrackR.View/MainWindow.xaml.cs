bdd976395ed1ffe929f00c2963bd48be589f395d
Koncz16 <koncz.hunor@yahoo.com>
2024-07-01 00:08:28 +0300

    Added functionality for exporting data to Excel and CSV formats

--- TM.DailyTrackR.View/MainWindow.xaml.cs
+++ TM.DailyTrackR.View/MainWindow.xaml.cs
@@ -10,6 +10,9 @@
     using TM.DailyTrackR.DataType.Models;
     using System.IO;
     using TM.DailyTrackR.DataType.Enums;
+    using System.Text;
+    using ClosedXML.Excel;
+    using System.Collections.Generic;
 
     public partial class MainWindow : Window
   {
@@ -19,7 +22,108 @@
             InitializeComponent();
             DataContext = new MainWindowViewModel();
         }
-        private void ExportToFile(object sender, RoutedEventArgs e)
+
+        private void DailyDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+
+        private void ExportToCsv(object sender, RoutedEventArgs e)
+        {
+            //var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            //{
+            //    Filter = "CSV Files (*.csv)|*.csv",
+            //    DefaultExt = ".csv"
+            //};
+            if (textBlock.Text != "")
+            {
+
+                    var startDate = SelectorCalendar.SelectedDates.Min();
+                var endDate = SelectorCalendar.SelectedDates.Max();
+                var activitiesRange = LogicHelper.Instance.ExampleController.GetActivitiesBetweenDates(startDate, endDate);
+                var filename = $"TeamWeekActivity_{startDate.ToString("dd.MM.yyyy")}_{endDate.ToString("dd.MM.yyyy")}.csv";
+                string relativePath = $"{filename}";
+                string fullPath = System.IO.Path.Combine(@"D:\Topmotive\tmDailyTrackR\TM.DailyTrackR\TM.DailyTrackR", relativePath);
+
+                ExportToCsv(fullPath, activitiesRange);
+                MessageBox.Show("Data successfully exported into CVS!", "Export Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a period to export.", "Period Not Selected", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+}
+
+
+        public void ExportToCsv(string filePath, IEnumerable<ActivityModel> data)
+        {
+            var csvBuilder = new StringBuilder();
+            csvBuilder.AppendLine("Project Type,Task Type,Description,Status");
+
+            foreach (var item in data)
+            {
+                csvBuilder.AppendLine($"{item.ProjectTypeDescription},{item.ActivityTypeId},{item.Description},{item.StatusId}");
+            }
+
+            File.WriteAllText(filePath, csvBuilder.ToString());
+        }
+
+
+        private void ExportToExcel(object sender, RoutedEventArgs e)
+        {
+            //var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            //{
+            //    Filter = "Excel Files (*.xlsx)|*.xlsx",
+            //    DefaultExt = ".xlsx"
+            //};
+
+            if (textBlock.Text != "")
+            {
+                var startDate = SelectorCalendar.SelectedDates.Min();
+                var endDate = SelectorCalendar.SelectedDates.Max();
+                var activitiesRange = LogicHelper.Instance.ExampleController.GetActivitiesBetweenDates(startDate, endDate);
+                var filename = $"TeamWeekActivity_{startDate.ToString("dd.MM.yyyy")}_{endDate.ToString("dd.MM.yyyy")}.xlsx";
+                string relativePath = $"{filename}";
+                string fullPath = System.IO.Path.Combine(@"D:\Topmotive\tmDailyTrackR\TM.DailyTrackR\TM.DailyTrackR", relativePath);
+
+                ExportToExcel(fullPath, activitiesRange);
+                MessageBox.Show("Data successfully exported into excel!", "Export Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select a period to export.", "Period Not Selected", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+
+        public void ExportToExcel(string filePath, IEnumerable<ActivityModel> data)
+    {
+        var workbook = new XLWorkbook();
+        var worksheet = workbook.Worksheets.Add("Activities");
+
+        // Header
+        worksheet.Cell(1, 1).Value = "Project Type";
+        worksheet.Cell(1, 2).Value = "Task Type";
+        worksheet.Cell(1, 3).Value = "Description";
+        worksheet.Cell(1, 4).Value = "Status";
+
+        // Data
+        var row = 2;
+        foreach (var item in data)
+        {
+            worksheet.Cell(row, 1).Value = item.ProjectTypeDescription;
+            worksheet.Cell(row, 2).Value = item.ActivityTypeId.ToString();
+            worksheet.Cell(row, 3).Value = item.Description;
+            worksheet.Cell(row, 4).Value = item.StatusId.ToString();
+            row++;
+        }
+
+        workbook.SaveAs(filePath);
+    }
+
+
+
+    private void ExportToFile(object sender, RoutedEventArgs e)
         {
             if (textBlock.Text != "")
 
@@ -33,7 +137,7 @@
                 string fullPath = System.IO.Path.Combine(@"D:\Topmotive\tmDailyTrackR\TM.DailyTrackR\TM.DailyTrackR", relativePath);
 
                 ExportActivitiesToTxt(activitiesRange, fullPath, startDate, endDate);
-                MessageBox.Show("Data successfully exported!", "Export Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Data successfully exported into txt!", "Export Successful", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             else
             {
